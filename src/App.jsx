a3ea8fc9818e4be4e9aee41cfd173635f2c55613
Dina Torres <cliomei12@gmail.com>
2024-04-03 23:25:37 +0200

    App config con wagmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { AppLayout } from "./components/Ui/Layout";
-import { Home } from './pages';
+import { ConnectKitProvider } from 'connectkit'
+import { Toaster } from 'react-hot-toast'
+import { WagmiConfig } from 'wagmi'
+import { AppLayout } from './components/ui/layouts'
+import { config } from './config/wagmi'
+import { Home } from './pages'
 
 function App() {
   return (
-    <AppLayout>
-      <Home />
-    </AppLayout>   
-
+    <WagmiConfig config={config}>
+      <Toaster position="bottom-right" />
+      <ConnectKitProvider mode="light">
+        <AppLayout>
+          <Home />
+        </AppLayout>
+      </ConnectKitProvider>
+    </WagmiConfig>
   )
 }
 
